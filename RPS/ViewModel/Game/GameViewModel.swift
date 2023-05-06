b3f0728de82ab217e95233618dd168f7fa09f824
Carlos Martin <carlos.martin@softhouse.se>
2023-05-06 21:58:48 +0200

    Clean up GameViewModel

--- RPS/ViewModel/Game/GameViewModel.swift
+++ RPS/ViewModel/Game/GameViewModel.swift
@@ -14,7 +14,6 @@ class GameViewModel: ObservableObject {
     @Published var game: Game
 
     private var myId: String
-    private var firstTime: Bool
 
     init(game: Game, me: Player) {
         self.onError = false
@@ -22,39 +21,5 @@ class GameViewModel: ObservableObject {
         self.playerOne = game.playerOneInGame(myId: me.id)
         self.playerTwo = game.playerTwoInGame(myId: me.id)
         self.myId = me.id
-        self.firstTime = true
-    }
-
-    func checkingGame() {
-        GameService.sharedInstance
-            .fetchGame(id: game.id) { [weak self] game, error in
-                guard let self = self, let game = game else {
-                    self?.onError(error)
-                    return
-                }
-                onSuccess(game)
-
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    self.checkingGame()
-                }
-            }
-    }
-
-    private func onError(_ error: Error?) {
-        printlog(String(describing: error))
-        DispatchQueue.main.async {
-            self.onError = true
-        }
-    }
-
-    private func onSuccess(_ game: Game) {
-        DispatchQueue.main.async {
-            if self.game != game {
-                self.game = game
-                self.playerOne = game.playerOneInGame(myId: self.myId)
-                self.playerTwo = game.playerTwoInGame(myId: self.myId)
-                printlog(game.toJson() ?? "")
-            }
-        }
     }
 }
